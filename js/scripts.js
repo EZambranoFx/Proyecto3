e87de3c71ae753e7506d09e9520028950f0bacee
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-06-17 23:59:58 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,53 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Obtiene todos los elementos de producto
+var products = document.getElementsByClassName("product");
+
+// Agrega un controlador de eventos a cada producto
+Array.from(products).forEach(function (product) {
+    var productName = product.querySelector("h3").textContent;
+    var productDescription = product.querySelector("p").textContent;
+
+    product.addEventListener("click", function () {
+        var isActive = product.classList.contains("active");
+
+        // Quita la clase "active" de todos los productos
+        Array.from(products).forEach(function (prod) {
+            prod.classList.remove("active");
+            prod.querySelector("img").classList.remove("selected");
+        });
+
+        if (!isActive) {
+            // Agrega la clase "active" al producto seleccionado
+
+            product.querySelector("img").classList.add("selected");
+            displayProductInfo(productName, productDescription);
+        } else {
+            hideProductInfo();
+        }
+    });
+});
+
+function displayProductInfo(name, description) {
+    var productInfo = document.getElementById("product-info");
+    var productName = document.getElementById("product-name");
+    var productDescription = document.getElementById("product-description");
+
+    productName.textContent = name;
+    productDescription.textContent = description;
+    productInfo.style.display = "block";
+}
+
+function hideProductInfo() {
+    var productInfo = document.getElementById("product-info");
+    productInfo.style.display = "none";
+}
+
+var contador=0;
+function cambiarTexto() {
+    var span = document.getElementById("contador");
+    contador++;
+    span.textContent = contador;
+}; 
